dcdfc9704dd96f59f39cd3184349ce7b3e6483eb
danilovalerio <danilo_vs@hotmail.com>
2023-08-30 16:44:23 -0300

    Base do projeto

---
+++ src/test/java/com/me/dio/mybank/danilo/santander/dev/week3/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.dio.mybank.danilo.santander.dev.week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
